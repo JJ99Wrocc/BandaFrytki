6e58d6d700e46e1030273ed886a077c8addb3f16
Iwona Esangbedo <esangbedojoachim@gmail.com>
2026-04-20 13:12:52 +0200

    prepare a enviroment for a render hosting

--- src/components/OrderFinalization.js
+++ src/components/OrderFinalization.js
@@ -76,7 +76,7 @@ const OrderFinalization = ({ selectedSize, shake, setShake, onSuccess, onBack })
         }
 
         try {
-            const response = await fetch('http://localhost:5000/api/orders', {
+            const response = await fetch('https://bandafrytki.onrender.com/api/orders', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
